7e8c72cd07dd51ee300a2bba97ce0d25c12b5da5
Minghan Li <160384038+limingham@users.noreply.github.com>
2026-01-05 00:51:20 +0800

    Add files via upload

---
+++ fig2/deconvolution_code/psedobulk_ER.R
@@ -0,0 +1,129 @@
+library(Seurat)
+library(fastSave)
+
+
+bulk_data <- readRDS("~/Benchmark-realdata-main/fig2/data_preprocess/psedobulk.rds")
+sc_ER = readRDS.lbzip2('~/Benchmark-realdata-main/fig2/data_preprocess/seurat_use.rdsFS')
+sc_ER = subset(sc_ER,subtype == 'ER+')
+
+
+inter_name = intersect(colnames(bulk_data),unique(sc_ER@meta.data$orig.ident))
+
+
+for(sample in inter_name){
+  
+  
+  ER_bulk = as.data.frame(bulk_data[[sample]])
+  # ER_bulk[,2] = ER_bulk[,1]
+  # row.names(ER_bulk) = row.names(bulk_data)
+  seurat_ER = subset(sc_ER,orig.ident != sample)
+  # ER_bulk[] <- lapply(ER_bulk, function(x) as.integer(as.character(x)))
+  
+  
+  results_path <- paste0("~/Benchmark-realdata-main/fig2/deconvolution_results/ER_pseudobulk/",sample)
+  if (dir.exists(results_path)) {
+  } else {
+    dir.create(results_path)
+  }
+  source("/home/lmh/ReCIDE_git/ReCIDE/R/ReCIDE_all_function_beta.R")
+  source("/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R")
+  
+  # 
+  # 
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], " ", "_")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], "\\/", "_")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], "\\.", "_")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], "\\(", "")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], "\\)", "")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], ",", "_")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace(seurat_ER@meta.data[["celltype_use"]], "-", "")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace(seurat_ER@meta.data[["celltype_use"]], "\\+", "")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace(seurat_ER@meta.data[["celltype_use"]], "_III", "3")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace(seurat_ER@meta.data[["celltype_use"]], "_II", "2")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace(seurat_ER@meta.data[["celltype_use"]], "_I", "1")
+  seurat_ER@meta.data[["celltype_use"]] <- str_replace_all(seurat_ER@meta.data[["celltype_use"]], "\\-", "_")
+  
+  
+  
+  celltype1 = "celltype_use"
+  subject1 = "orig.ident"
+  
+  dir_ref_recide = paste0(results_path,'/ref_ReCIDE.rds')
+  dir_results_recide = paste0(results_path,'/results_ReCIDE.rds')
+  dir_results_bisque = paste0(results_path,'/results_bisque.rds')
+  dir_results_music = paste0(results_path,'/results_music.rds')
+  dir_results_bayes = paste0(results_path,'/results_bayes.rds')
+  dir_ref_CIBERSORT = paste0(results_path,'/ref_CIBERSORT.rds')
+  dir_results_CIBERSORT = paste0(results_path,'/results_CIBERSORT.txt')
+  dir_ref_DWLS = paste0(results_path,'/ref_DWLS.rds')
+  dir_results_DWLS = paste0(results_path,'/results_DWLS.rds')
+
+  ######run_ReCIDE
+  #
+  source('/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R')
+  func_run_ReCIDE(SC_ref = seurat_ER,
+                  EXP_df = ER_bulk,
+                  celltype = celltype1,
+                  subject = subject1,
+                  dir_ref = dir_ref_recide,
+                  dir_results = dir_results_recide)
+
+
+  ######run_bisque
+  source('/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R')
+
+  func_run_bisque(ref_seurat = seurat_ER,
+                  bulkdata = ER_bulk,
+                  celltype = celltype1,
+                  subject = subject1,
+                  dir_results = dir_results_bisque)
+  gc()
+
+  ######run_music
+  source('/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R')
+
+  func_run_music(ref_seurat = seurat_ER,
+                 EXP_df = ER_bulk,
+                 celltype = celltype1,
+                 subject = subject1,
+                 dir_results = dir_results_music)
+  gc()
+
+
+
+
+  ######run_CIBERSORT
+  source('/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R')
+
+  func_run_CIBERSORT(combined.data = seurat_ER,
+                     bulk.mtx = ER_bulk,
+                     celltype = celltype1,
+                     subject = subject1,
+                     dir_ref = dir_ref_CIBERSORT,
+                     dir_results = dir_results_CIBERSORT)
+  gc()
+
+
+  ######run_DWLS
+  source('/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R')
+
+  func_run_DWLS(scdata_test = seurat_ER,
+                bulkdata = ER_bulk,
+                celltype = celltype1,
+                dir_DWLS_ref = dir_ref_DWLS,
+                dir_DWLS_results = dir_results_DWLS)
+  gc()
+
+  # 
+  ######run_bayes
+  source('/home/lmh/ReCIDE/benchmark_syq/main_run_all_revise.R')
+  
+  func_run_bayes(ref_seurat = seurat_ER,
+                 bulk.mtx = ER_bulk,
+                 celltype = celltype1,
+                 subject = subject1,
+                 dir_results = dir_results_bayes,
+                 key1 = 'Epithelial')
+  # rm(list = ls())
+  gc()
+}
---
+++ fig2/results_analysis/fig/F2A/F2A.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(reshape2)
+library(ggpubr)
+
+source("~/Benchmark-realdata-main/fig2/results_analysis/fig/F2A/preprocess.R")
+
+
+pcc_pseudo_BRCA_df$sample = row.names(pcc_pseudo_BRCA_df)
+pcc_real_BRCA_df$sample = row.names(pcc_real_BRCA_df)
+
+
+pcc_psedobulk <- melt(pcc_pseudo_BRCA_df, 
+                      id.vars = "sample",        
+                      variable.name = "Method",  
+                      value.name = "Value")    
+pcc_psedobulk$category = 'psedobulk'
+
+pcc_realbulk <- melt(pcc_real_BRCA_df, 
+                     id.vars = "sample",       
+                     variable.name = "Method",  
+                     value.name = "Value")    
+pcc_realbulk$category = 'realbulk'
+
+
+plot_df = rbind(pcc_psedobulk,pcc_realbulk)
+
+pcc_psedobulk %>% group_by(Method) %>% summarise_each(mean)
+pcc_realbulk %>% group_by(Method) %>% summarise_each(mean)
+
+plot_df$Method = as.character(plot_df$Method)
+p = ggplot(plot_df,aes(x = Method,y = Value,fill = category)) +
+  geom_boxplot(aes(color = category),
+               width = .6, size = .7, alpha = .5 ) +
+  theme_classic() +
+  scale_color_manual(values= c('#6793ea','#ca7491')) +
+  scale_fill_manual(values= c('#6793ea','#ca7491')) +
+  theme(
+    axis.text.x = element_text(size = 14, face = "plain", angle = -45),
+    axis.text.y = element_text(size = 14, face = "plain"),
+    # axis.text.x = element_blank(),
+    # axis.text.y = element_blank(),
+    axis.title = element_text(size = 14, face = "plain"),
+    plot.title = element_text(size = 14, face = "plain", hjust = 0.5),
+    plot.subtitle = element_text(size = 14, face = "plain", hjust = 0.5),
+    panel.border = element_rect(fill=NA,color="black", size=1, linetype="solid"),
+    legend.text = element_text(size = 14),
+    legend.spacing.x = unit(0.4, "cm"),
+    # axis.title = element_text(size = 8)
+    legend.position = "bottom"
+  )+
+  stat_compare_means(method = 'wilcox.test',paired = TRUE,label = "p.signif")
+
+p
+ggsave(
+  filename = "~/Benchmark-realdata-main/fig2/results_analysis/fig/pdf/F2A.pdf",  
+  plot = p,  
+  width = 5,  
+  height = 4,  
+  units = "in",  
+  dpi = 300,  
+  device = "pdf"  
+)
